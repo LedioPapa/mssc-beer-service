53c94b277589e2c125243774c9aa0ba04d1ba835
ledio.papa@fti.edu.al <n25I^kW4bfhW>
2020-04-30 00:27:18 +0200

    created data model for beer service. closes #2

---
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,8 @@
+package guru.springframework.msscbeerservice.web.model;
+
+/**
+ * Created by ledio on 4/30/20
+ */
+public enum BeerStyleEnum {
+  LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
